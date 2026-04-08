5290226b341313cc9e02a7ffb5e3bb061224a7d9
whospavaniii25 <pavanisowmyaa6@gmail.com>
2026-04-08 20:25:18 +0530

    UC1

---
+++ App/src/BookMyStay.java
@@ -0,0 +1,15 @@
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("Welcome to Hotel Booking Management System");
+
+        // Application name and version
+        System.out.println("Application: BookMyStay");
+        System.out.println("Version: 1.0");
+
+        // Confirmation message
+        System.out.println("System started successfully.");
+    }
+}
